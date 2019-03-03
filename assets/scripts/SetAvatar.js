f2f9ab4fbf5b871a8b3825f6478c78efe77d0cce
zcsd <zichun92@gmail.com>
2019-03-03 14:06:47 +0800

    load assets dynamicly

--- assets/scripts/SetAvatar.js
+++ assets/scripts/SetAvatar.js
@@ -2,12 +2,35 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        changeButton: {
+            default: null,
+            type: cc.Button
+        },
 
+        nameEditBox: {
+            default: null,
+            type: cc.EditBox
+        },
+
+        genderToggleContainer: {
+            default: null,
+            type: cc.ToggleContainer
+        },
+
+        frontSprite: {
+            default: null,
+            type: cc.Sprite
+        },
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {},
+
+    changeAvatar: function () {
+        cc.log('change avatar function');
+        //this.frontSprite.spriteFrame.setTexture(cc.loader.load('res/boy.png'));
+    },
 
     start () {
 
