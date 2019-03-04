cc093fb5e012342712d80e5a17a20b9eba919890
zcsd <zichun92@gmail.com>
2019-03-04 18:32:09 +0800

    press to change avatar

--- assets/scripts/SetAvatar.js
+++ assets/scripts/SetAvatar.js
@@ -21,21 +21,46 @@ cc.Class({
             default: null,
             type: cc.Sprite
         },
+
+        currentGender: "boy",
+        currentImgPosition: 0,
+
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad: function () {
+        //console.log(boyAvatarsDir[0]);
         // load default boy avatar image
         var self = this;
         cc.loader.loadRes("boy_0", cc.SpriteFrame, function (err, spriteFrame) {
             self.frontSprite.spriteFrame = spriteFrame;
         });
+
     },
 
     changeAvatar: function () {
+        let boyAvatarsDir = ["boy_0", "boy_1", "boy_2"];
+        let girlAvatarsDir = ["girl_0", "girl_1", "girl_2"];
+
+        this.currentImgPosition += 1;
+        if (this.currentImgPosition == boyAvatarsDir.length) {
+            this.currentImgPosition = 0;
+        }
+
+        let currentImgDir = "";
+        if (this.currentGender == "boy") {
+            currentImgDir = boyAvatarsDir[this.currentImgPosition];
+        }
+        else if (this.currentGender == "girl") {
+            currentImgDir = girlAvatarsDir[this.currentImgPosition];
+        }
+        else {
+            console.log("Select Gender Wrongly.");
+        } 
+        
         var self = this;
-        cc.loader.loadRes("girl_2", cc.SpriteFrame, function (err, spriteFrame) {
+        cc.loader.loadRes(currentImgDir, cc.SpriteFrame, function (err, spriteFrame) {
             self.frontSprite.spriteFrame = spriteFrame;
         });
     },
