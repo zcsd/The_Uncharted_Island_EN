63e4dc9f4cd08588d1eb3eefb0ea3d505d598e0a
zcsd <zichun92@gmail.com>
2019-03-27 18:00:40 +0800

    add sprite action

--- assets/scripts/SaveBananaTree.js
+++ assets/scripts/SaveBananaTree.js
@@ -17,6 +17,14 @@ cc.Class({
             type: cc.Sprite
         },
 
+        spriteScale: 1,
+        spritePos: cc.v2(-435, -190),
+        leftLimit: -435,
+        rightLimit: 300,
+        maxSpriteScale: 1.0,
+        minSpriteScale: 0.2,
+        stepSize: 30,
+
     },
 
     // LIFE-CYCLE CALLBACKS:
@@ -39,10 +47,58 @@ cc.Class({
 
     },
 
+    moveToLeft: function () {
+        //var spriteNode = cc.find('Canvas/moveSprite');
+        //spriteNode.position = this.spritePos;
+        let x = this.spritePos.x;
+        x -= this.stepSize * this.spriteScale;
+
+        if (x <= this.leftLimit) {
+            x = this.leftLimit
+        }
+
+        this.spritePos = cc.v2(x, this.spritePos.y);
+        this.moveSprite.node.position = cc.v2(this.spritePos);
+    },
+
+    moveToRight: function () {
+        let x = this.spritePos.x;
+        x += this.stepSize * this.spriteScale;
+
+        if (x >= this.rightLimit) {
+            x = this.rightLimit
+        }
+
+        this.spritePos = cc.v2(x, this.spritePos.y);
+        this.moveSprite.node.position = cc.v2(this.spritePos);
+    },
+
+    increaseSize: function () {
+        let currentScale = this.moveSprite.node.scale;
+
+        currentScale += 0.1;
+        if (currentScale > this.maxSpriteScale) {
+            currentScale = this.maxSpriteScale;
+        }
+        this.spriteScale = currentScale;
+        this.moveSprite.node.scale = this.spriteScale;
+    },
+
+    decreaseSize: function() {
+        let currentScale = this.moveSprite.node.scale;
+
+        currentScale -= 0.1;
+        if (currentScale < this.minSpriteScale) {
+            currentScale = this.minSpriteScale;
+        }
+        this.spriteScale = currentScale;
+        this.moveSprite.node.scale = this.spriteScale;
+    },
+
     backToMapScene: function () {
         cc.director.loadScene("LevelMap");
 
     },
 
-    // update (dt) {},
+    update (dt) {},
 });
