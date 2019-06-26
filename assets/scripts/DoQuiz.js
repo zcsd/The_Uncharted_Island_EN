78b5ce99f9c06266380b9ecf81689a126330a75d
zcsd <zichun92@gmail.com>
2019-06-26 17:27:05 +0800

    add json mcq

--- assets/scripts/DoQuiz.js
+++ assets/scripts/DoQuiz.js
@@ -14,9 +14,25 @@ cc.Class({
             type: cc.Sprite
         },
 
-        answerChoice: 1,
+        userAnswerChoice: 1, // user choice
+
+        questionLabel: cc.Label,
+
+        option1Label: cc.Label,
+
+        option2Label: cc.Label,
+
+        option3Label: cc.Label,
+
+        option4Label: cc.Label,
+
+        currentOrder: 1,
 
         answerToggleContainer: cc.ToggleContainer,
+
+        mcqData: cc.JsonAsset,
+
+        currentAnswerChoice: 1, // correct choice
     },
 
     // LIFE-CYCLE CALLBACKS:
@@ -31,6 +47,16 @@ cc.Class({
         cc.loader.loadRes(player.avatarImgDir + '_s', cc.SpriteFrame, function (err, spriteFrame) {
             self.avatarSprite.spriteFrame = spriteFrame;
         });
+        // load mcq data from json
+        cc.loader.loadRes('mcq', function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                self.mcqData = data;
+                self.loadMCQ(self.currentOrder);
+            }
+        });
 
     },
 
@@ -38,8 +64,40 @@ cc.Class({
 
     },
 
+    loadMCQ: function (order) {
+        this.questionLabel.string = this.mcqData.json[order].question;
+        this.option1Label.string = this.mcqData.json[order].option1;
+        this.option2Label.string = this.mcqData.json[order].option2;
+        this.option3Label.string = this.mcqData.json[order].option3;
+        this.option4Label.string = this.mcqData.json[order].option4;
+
+        //cc.find('Canvas/answerToggleContainer/toggle4/checkmark').active = false;
+        //cc.find('Canvas/answerToggleContainer/toggle1/checkmark').active = true;
+    },
+
+    submitAnswer: function () {
+        var player = cc.find('player').getComponent('Player');
+        
+        if (this.userAnswerChoice == this.mcqData.json[this.currentOrder].answerOrder) {
+            console.log("答对了");
+            player.coinsOwned += 50;
+            this.coinLabel.string = player.coinsOwned.toString();
+        }
+        else {
+            console.log("答错了");
+        }
+        
+        cc.find("Canvas/submitButton").getComponent(cc.Button).interactable = false;
+    },
+
+    goToNextQuestion: function () {
+        this.currentOrder += 1;
+        this.loadMCQ(this.currentOrder);
+        cc.find("Canvas/submitButton").getComponent(cc.Button).interactable = true;
+    },
+
     changeAnswer: function (event, customEventData) {
-        this.answerChoice = event.node._name.replace('toggle', '');
+        this.userAnswerChoice = event.node._name.replace('toggle', '');
     },
 
     backToMapScene: function () {
