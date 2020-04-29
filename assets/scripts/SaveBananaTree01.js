c36a10d4239253ccec3b737d18df6544e32d4241
zcsd <zichun92@gmail.com>
2020-04-29 10:14:22 +0800

    add bana2 flow

--- assets/scripts/SaveBananaTree01.js
+++ assets/scripts/SaveBananaTree01.js
@@ -4,13 +4,15 @@ cc.Class({
     properties: {
         nameLabel: cc.Label,
         coinLabel: cc.Label,
+        hintLabel: cc.Label,
         avatarSprite: cc.Sprite,
 
         coinRotate: cc.Node,
         coinShine: cc.Node,
         coinBlink: cc.Node,
 
-        binRead: 0b000,
+        binRead: 0b000, //how many holes read
+        numCollect: 0,
     },
 
     onLoad: function () {
@@ -32,7 +34,9 @@ cc.Class({
 
     selectFreeDiff: function(){
         console.log("自由扩散");
-        Alert.show(1.4, "自由扩散", "自由扩散是小分子物质从浓度高的一侧通过细胞膜向浓度低的一侧转运，不需要载体，不需要消耗能量，属于被动运输的一种，是最简单的运输方式之一。", null, false);
+        Alert.show(1.3, "自由扩散", "自由扩散是小分子物质从浓度高的一侧通过细胞膜向浓度低的一侧转运，不需要载体，不需要消耗能量，属于被动运输的一种，是最简单的运输方式之一。", null, false);
+
+        cc.find("Canvas/freeDiffHole/Background").stopAllActions();
 
         if ((this.binRead & 0b001) == 0b000) {
             this.binRead = this.binRead | 0b001;
@@ -45,8 +49,10 @@ cc.Class({
 
     selectFaciDiff: function(){
         console.log("协助扩散");
-        Alert.show(1.4, "协助扩散", "协助扩散也称促进扩散，是大分子物质从浓度高的一侧通过细胞膜上的特殊载体向浓度低的一侧转运，不需要消耗能量，属于被动运输的一种。", null, false);
+        Alert.show(1.3, "协助扩散", "协助扩散也称促进扩散，是大分子物质从浓度高的一侧通过细胞膜上的特殊载体向浓度低的一侧转运，不需要消耗能量，属于被动运输的一种。", null, false);
         
+        cc.find("Canvas/faciDiffHole/Background").stopAllActions();
+
         if ((this.binRead & 0b010) == 0b000) {
             this.binRead = this.binRead | 0b010;
         }
@@ -58,13 +64,36 @@ cc.Class({
 
     selectActTran: function(){
         console.log("主动运输");
-        Alert.show(1.4, "主动运输", "主动运输是物质从浓度低的一侧通过细胞膜上的特殊载体向浓度高的一侧转运, 需要消耗能量，常见的有钠离子钾离子进出细胞膜。", null, false);
+        Alert.show(1.3, "主动运输", "主动运输是物质从浓度低的一侧通过细胞膜上的特殊载体向浓度高的一侧转运, 需要消耗能量，常见的有钠离子钾离子进出细胞膜。", null, false);
+        
+        cc.find("Canvas/actTranHole/Background").stopAllActions();
+        
         if ((this.binRead & 0b100) == 0b000) {
             this.binRead = this.binRead | 0b100;
         }
         console.log(this.binRead);
         if((this.binRead & 0b111) == 0b111) {
             this.setMolecueStatus(true);
+        }
+    },
+
+    collectMolecues: function(event, btnName) {
+        this.hintLabel.string = "开始点击水分子和无机盐来收集它们吧，植物生长需要它们。";
+        cc.find(btnName).active = false;
+        cc.find(btnName).getComponent(cc.Button).interactable = false;
+        cc.find("Canvas/collectBox").active = true;
+        if(btnName.includes("salt")){
+            cc.find("Canvas/minSaltOwned").active = true;
+            var seq = cc.sequence(cc.scaleBy(0.5, 1.2), cc.scaleBy(0.5, 0.833), cc.scaleBy(0.5, 1.11));
+            cc.find("Canvas/minSaltOwned").runAction(seq);
+        }else if(btnName.includes("h2o")){
+            cc.find("Canvas/h2oOwned").active = true;
+            var seq = cc.sequence(cc.scaleBy(0.5, 1.2), cc.scaleBy(0.5, 0.833), cc.scaleBy(0.5, 1.11));
+            cc.find("Canvas/h2oOwned").runAction(seq);
+        }
+        this.numCollect += 1;
+        if(this.numCollect >= 7 ){
+            this.hintLabel.string = "收集完成，现在带着水和无机盐准备进入根毛里面吧!";
         }
     },
 
